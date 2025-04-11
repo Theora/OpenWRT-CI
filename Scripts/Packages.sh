595ac28bc8082df8dae2870c777f96c50d08e63c
Theora <140498591+Theora@users.noreply.github.com>
2025-04-11 17:20:04 +0700

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -56,7 +56,6 @@ UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
 
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 UPDATE_PACKAGE "diskman" "lisaac/luci-app-diskman​" "master"
-UPDATE_PACKAGE "vlmcsd" "cokebar/luci-app-vlmcsd" "master"
 
 UPDATE_PACKAGE "alist" "sbwml/luci-app-alist" "main"
 UPDATE_PACKAGE "easytier" "EasyTier/luci-app-easytier" "main"
