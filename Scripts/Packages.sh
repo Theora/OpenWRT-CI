833a56fc9da1e80157e59f273b58f716ac445124
Theora <140498591+Theora@users.noreply.github.com>
2025-04-12 09:55:34 +0700

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -56,9 +56,9 @@ UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
 UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
 
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
-UPDATE_PACKAGE "luci-app-diskman" "lisaac/luci-app-diskman​" "master"
+UPDATE_PACKAGE "luci-app-diskman" "lisaac/luci-app-diskman" "master"
 
-UPDATE_PACKAGE "luci-app-alist" "sbwml/luci-app-alist" "main"
+UPDATE_PACKAGE "alist" "sbwml/luci-app-alist" "main"
 UPDATE_PACKAGE "easytier" "EasyTier/luci-app-easytier" "main"
 UPDATE_PACKAGE "gecoosac" "lwb1978/openwrt-gecoosac" "main"
 UPDATE_PACKAGE "mosdns" "sbwml/luci-app-mosdns" "v5" "" "v2dat"
